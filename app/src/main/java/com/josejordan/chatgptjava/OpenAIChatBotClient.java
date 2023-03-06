58ec21fa8191c02df6fd687410b84866ab902082
JordanPro <mundodigital.pro@gmail.com>
2023-03-06 17:58:03 +0100

    implementing okhttp

--- app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
+++ app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
@@ -8,18 +8,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class OpenAIChatBotClient {
 
@@ -29,6 +28,9 @@ public class OpenAIChatBotClient {
     private static final int MAX_TOKENS = 12;
     private static final int TEMPERATURE = 0;
 
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    private final OkHttpClient client = new OkHttpClient();
     private final Executor executor = Executors.newSingleThreadExecutor();
     private final Handler handler = new Handler(Looper.getMainLooper());
 
@@ -36,12 +38,6 @@ public class OpenAIChatBotClient {
         executor.execute(() -> {
             String response = "";
             try {
-                URL url = new URL("https://api.openai.com/v1/chat/completions");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setRequestProperty("Authorization", "Bearer " + API_KEY);
-
                 JSONObject postFields = new JSONObject();
                 postFields.put("model", MODEL_NAME);
 
@@ -56,27 +52,26 @@ public class OpenAIChatBotClient {
                 postFields.put("max_tokens", MAX_TOKENS);
                 postFields.put("temperature", TEMPERATURE);
 
-                connection.setDoOutput(true);
-                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8))) {
-                    writer.write(postFields.toString());
-                }
+                RequestBody requestBody = RequestBody.create(JSON, postFields.toString());
+                Request request = new Request.Builder()
+                        .url("https://api.openai.com/v1/chat/completions")
+                        .addHeader("Authorization", "Bearer " + API_KEY)
+                        .post(requestBody)
+                        .build();
 
-                int responseCode = connection.getResponseCode();
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                    StringBuilder responseBuilder = new StringBuilder();
-                    String inputLine;
-                    while ((inputLine = reader.readLine()) != null) {
-                        responseBuilder.append(inputLine);
+                try (Response responseHttp = client.newCall(request).execute()) {
+                    if (!responseHttp.isSuccessful()) {
+                        throw new IOException("Unexpected response code: " + responseHttp);
                     }
-                    JSONObject jsonResponse = new JSONObject(responseBuilder.toString());
+                    JSONObject jsonResponse = new JSONObject(responseHttp.body().string());
                     response = jsonResponse.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
-                } catch (JSONException e) {
+                } catch (IOException | JSONException e) {
                     Log.e(TAG, "Error parsing JSON response", e);
                 }
-                connection.disconnect();
-            } catch (IOException | JSONException e) {
-                Log.e(TAG, "Error sending request to OpenAI ChatBot API", e);
+            } catch (JSONException e) {
+                Log.e(TAG, "Error creating JSON object", e);
             }
+
             String finalResponse = response;
             handler.post(() -> listener.onResponse(finalResponse));
         });
