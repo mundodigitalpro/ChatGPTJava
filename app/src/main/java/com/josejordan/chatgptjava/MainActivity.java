3a09c850a5045ddbdf7e9769f19ab5e955b52b60
JordanPro <mundodigital.pro@gmail.com>
2023-03-07 11:34:19 +0100

    layout updated

--- app/src/main/java/com/josejordan/chatgptjava/MainActivity.java
+++ app/src/main/java/com/josejordan/chatgptjava/MainActivity.java
@@ -1,24 +1,38 @@
 package com.josejordan.chatgptjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText etText;
+    private TextView tvText;
+    private OpenAIChatBotClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String query = "What is the capital city of England?";
-        OpenAIChatBotClient client = new OpenAIChatBotClient();
-        client.sendRequest(query, response -> {
-            // Aquí puedes procesar la respuesta del bot de chat
-            Log.i(TAG, "Chatbot response: " + response);
+        etText = findViewById(R.id.etText);
+        tvText = findViewById(R.id.tvText);
+
+        client = new OpenAIChatBotClient();
+
+        findViewById(R.id.btnSend).setOnClickListener(view -> {
+            String query = etText.getText().toString();
+            if (!query.isEmpty()) {
+                client.sendRequest(query, response -> {
+                    // Aquí puedes procesar la respuesta del bot de chat
+                    Log.i(TAG, "Chatbot response: " + response);
+                    tvText.setText(response);
+                });
+            }
         });
     }
 }
