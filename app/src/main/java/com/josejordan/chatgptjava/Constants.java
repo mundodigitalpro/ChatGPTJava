b6dd205aaeac6a468aa9b92e4a54d05d8d3e2c3a
JordanPro <mundodigital.pro@gmail.com>
2023-03-07 10:54:18 +0100

    created constans file and class update

---
+++ app/src/main/java/com/josejordan/chatgptjava/Constants.java
@@ -0,0 +1,9 @@
+package com.josejordan.chatgptjava;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.openai.com/v1/";
+    public static final String API_KEY = "YOUAPIKEY";
+    public static final String MODEL_NAME = "gpt-3.5-turbo";
+    public static final int MAX_TOKENS = 12;
+    public static final int TEMPERATURE = 0;
+}
--- app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
+++ app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
@@ -1,85 +1,10 @@
 package com.josejordan.chatgptjava;
-/*
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.io.IOException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-
-public class OpenAIChatBotClient {
-
-    private static final String TAG = OpenAIChatBotClient.class.getSimpleName();
-    private static final String API_KEY = "CHATGPTAPIKEY";
-    private static final String MODEL_NAME = "gpt-3.5-turbo";
-    private static final int MAX_TOKENS = 12;
-    private static final int TEMPERATURE = 0;
-    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    private final OkHttpClient client = new OkHttpClient();
-    private final Executor executor = Executors.newSingleThreadExecutor();
-    private final Handler handler = new Handler(Looper.getMainLooper());
-
-    public void sendRequest(String query, OnResponseListener listener) {
-        executor.execute(() -> {
-            String response = "";
-            try {
-                JsonObject postFields = new JsonObject();
-                postFields.addProperty("model", MODEL_NAME);
-
-                JsonArray messagesArr = new JsonArray();
-                JsonObject messageObj = new JsonObject();
-                messageObj.addProperty("role", "user");
-                messageObj.addProperty("content", query);
-                messagesArr.add(messageObj);
-
-                postFields.add("messages", messagesArr);
-                postFields.addProperty("max_tokens", MAX_TOKENS);
-                postFields.addProperty("temperature", TEMPERATURE);
-
-                RequestBody requestBody = RequestBody.create(postFields.toString(), JSON);
-
-                Request request = new Request.Builder()
-                        .url("https://api.openai.com/v1/chat/completions")
-                        .addHeader("Authorization", "Bearer " + API_KEY)
-                        .post(requestBody)
-                        .build();
-
-
-                try (Response responseHttp = client.newCall(request).execute()) {
-                    if (!responseHttp.isSuccessful()) {
-                        throw new IOException("Unexpected response code: " + responseHttp);
-                    }
-
-                    assert responseHttp.body() != null;
-                    JsonObject jsonResponse = JsonParser.parseString(responseHttp.body().string()).getAsJsonObject();
-                    response = jsonResponse.getAsJsonArray("choices").get(0).getAsJsonObject().getAsJsonObject("message").get("content").getAsString();
-
-                } catch (IOException e) {
-                    Log.e(TAG, "Error parsing JSON response", e);
-                }
-
-
-            } catch (Exception e) {
-                Log.e(TAG, "Error creating JSON object", e);
-            }
-            String finalResponse = response;
-            handler.post(() -> listener.onResponse(finalResponse));
-        });
-    }
-
-}*/
+import static com.josejordan.chatgptjava.Constants.API_KEY;
+import static com.josejordan.chatgptjava.Constants.BASE_URL;
+import static com.josejordan.chatgptjava.Constants.MAX_TOKENS;
+import static com.josejordan.chatgptjava.Constants.MODEL_NAME;
+import static com.josejordan.chatgptjava.Constants.TEMPERATURE;
 
 import android.os.Handler;
 import android.os.Looper;
@@ -97,14 +22,12 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
+
 public class OpenAIChatBotClient {
 
     private static final String TAG = OpenAIChatBotClient.class.getSimpleName();
-    private static final String BASE_URL = "https://api.openai.com/v1/";
-    private static final String API_KEY = "API_KEY";
-    private static final String MODEL_NAME = "gpt-3.5-turbo";
-    private static final int MAX_TOKENS = 12;
-    private static final int TEMPERATURE = 0;
+
     private final OpenAIApi openAIApi;
     private final Handler handler = new Handler(Looper.getMainLooper());
 
