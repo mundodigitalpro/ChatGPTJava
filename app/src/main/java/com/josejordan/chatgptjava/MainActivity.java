495faf3b930cc9ba7fccb57218dbf7d37ecc4284
JordanPro <mundodigital.pro@gmail.com>
2023-03-07 16:46:22 +0100

    text bubble layaout updated

--- app/src/main/java/com/josejordan/chatgptjava/MainActivity.java
+++ app/src/main/java/com/josejordan/chatgptjava/MainActivity.java
@@ -1,8 +1,11 @@
 package com.josejordan.chatgptjava;
 
+import static android.view.View.VISIBLE;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         etText = findViewById(R.id.etText);
         tvText = findViewById(R.id.tvText);
-
         client = new OpenAIChatBotClient();
+
 
         findViewById(R.id.btnSend).setOnClickListener(view -> {
             String query = etText.getText().toString();
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 client.sendRequest(query, response -> {
                     // Aquí puedes procesar la respuesta del bot de chat
                     Log.i(TAG, "Chatbot response: " + response);
+                    tvText.setVisibility(VISIBLE);
                     tvText.setTextColor(Color.WHITE);
                     tvText.setText(response);
                     etText.setText("");
