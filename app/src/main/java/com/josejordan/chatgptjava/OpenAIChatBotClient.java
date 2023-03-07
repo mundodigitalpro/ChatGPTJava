9b8359f961d9da49b2851164c22934537414a589
JordanPro <mundodigital.pro@gmail.com>
2023-03-07 09:52:20 +0100

    deleting comments

--- app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
+++ app/src/main/java/com/josejordan/chatgptjava/OpenAIChatBotClient.java
@@ -26,9 +26,7 @@ public class OpenAIChatBotClient {
     private static final String MODEL_NAME = "gpt-3.5-turbo";
     private static final int MAX_TOKENS = 12;
     private static final int TEMPERATURE = 0;
-
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-
     private final OkHttpClient client = new OkHttpClient();
     private final Executor executor = Executors.newSingleThreadExecutor();
     private final Handler handler = new Handler(Looper.getMainLooper());
@@ -37,32 +35,19 @@ public class OpenAIChatBotClient {
         executor.execute(() -> {
             String response = "";
             try {
-                //JSONObject postFields = new JSONObject();
-                //postFields.put("model", MODEL_NAME);
                 JsonObject postFields = new JsonObject();
                 postFields.addProperty("model", MODEL_NAME);
 
-//                List<JSONObject> messagesList = new ArrayList<>();
-//                JSONObject messageObj = new JSONObject();
-//                messageObj.put("role", "user");
-//                messageObj.put("content", query);
-//                messagesList.add(messageObj);
                 JsonArray messagesArr = new JsonArray();
                 JsonObject messageObj = new JsonObject();
                 messageObj.addProperty("role", "user");
                 messageObj.addProperty("content", query);
                 messagesArr.add(messageObj);
 
-//                JSONArray messagesArr = new JSONArray(messagesList);
-//                postFields.put("messages", messagesArr);
-//                postFields.put("max_tokens", MAX_TOKENS);
-//                postFields.put("temperature", TEMPERATURE);
                 postFields.add("messages", messagesArr);
                 postFields.addProperty("max_tokens", MAX_TOKENS);
                 postFields.addProperty("temperature", TEMPERATURE);
 
-
-                //RequestBody requestBody = RequestBody.create(JSON, postFields.toString());
                 RequestBody requestBody = RequestBody.create(postFields.toString(), JSON);
 
                 Request request = new Request.Builder()
@@ -77,9 +62,6 @@ public class OpenAIChatBotClient {
                         throw new IOException("Unexpected response code: " + responseHttp);
                     }
 
-                    //JSONObject jsonResponse = new JSONObject(responseHttp.body().string());
-                    //response = jsonResponse.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
-                    //JsonObject jsonResponse = new JsonParser().parse(responseHttp.body().string()).getAsJsonObject();
                     assert responseHttp.body() != null;
                     JsonObject jsonResponse = JsonParser.parseString(responseHttp.body().string()).getAsJsonObject();
                     response = jsonResponse.getAsJsonArray("choices").get(0).getAsJsonObject().getAsJsonObject("message").get("content").getAsString();
@@ -92,8 +74,6 @@ public class OpenAIChatBotClient {
             } catch (Exception e) {
                 Log.e(TAG, "Error creating JSON object", e);
             }
-
-            //handler.post(() -> listener.onResponse(finalResponse));
             String finalResponse = response;
             handler.post(() -> listener.onResponse(finalResponse));
         });
